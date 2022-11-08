bcd19dc50e9fa12182e22b32d129bcec6a6e2c9a
Lamozz <vanea20010@gmail.com>
2022-11-09 01:46:11 +0200

    Start Angular + Created home-page

---
+++ Forum.Angular/src/app/_services/category.service.ts
@@ -0,0 +1,34 @@
+import { Category } from '../_models/category/category';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>("/api/categories");
+  }
+
+  public getCategory(id: number): Observable<Category> {
+    return this.httpClient.get<Category>(`/api/categories/${id}`);
+  }
+
+  public createCategory(category: Category): Observable<Category> {
+    return this.httpClient.post<Category>("/api/categories", category);
+  }
+
+  public updateCategory(category: Category): Observable<Category> {
+    return this.httpClient.put<Category>(`/api/categories/${category.id}`, category);
+  }
+
+  public deleteCategory(id: number) {
+    return this.httpClient.delete(`/api/categories/${id}`);
+  }
+}
