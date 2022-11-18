3baba26036239afee2c84ffd79a64e7da009b967
Lamozz <vanea20010@gmail.com>
2022-11-18 18:41:13 +0200

    disabled nullable status

--- Forum.Domain/User.cs
+++ Forum.Domain/User.cs
@@ -2,7 +2,7 @@
 {
     public class User : BaseEntity
     {
-        public string? Status { get; set; }
+        public string Status { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
